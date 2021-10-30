3bb6a8dc6cdc535afedd08633da11ec421c47ffd
Manoj <manpane98@gmail.com>
2021-10-30 08:49:30 +0545

    proxy set to manoj9898.pythonaywhere.com

--- src/components/WebGames.jsx
+++ src/components/WebGames.jsx
@@ -1,26 +1,30 @@
 import {useEffect, useState } from "react";
 import axios from 'axios';
-const WebGames = (props) => {
+const WebGames = () => {
     const [games , setGames] = useState([]);
     const [games_div_class, setGamesDivClass] = useState("loading");
     useEffect(()=>{
-        axios.get('https://manoj9898.pythonanywhere.com/api/webgames').then(response => {
+        axios.get('/api/webgames').then(response => {
             console.log("Response : ",response);
             if (response){
-                setGames(response.data);
+                setGames(response.data.games);
+                setGamesDivClass("loaded");
             }
         }).catch(error=>{
             console.log("Error : ",error);
         });
     },[]);
-    if (games_div_class===""){setGamesDivClass("loading");}
     return ( 
-        <div className={"web-games-container "+games_div_class}>
-            <div className="progress"></div>
-            {games?.map((game)=>(
-                <a className="game-link" href={`http://manoj9898.pythonanywhere.com/games/${game}.html`}>{game}</a>
-            ))}
-        </div>
+        <center>
+            <div className={"web-games-container "+games_div_class}>
+                <div className="progress"/>
+                <div className="games-list">
+                    {games?.map((game)=>(
+                        <a className="game-link" href={`http://manoj9898.pythonanywhere.com/games/${game}.html`}>{game}</a>
+                    ))}
+                </div>
+            </div>
+        </center>
      );
 }
 export default WebGames;
