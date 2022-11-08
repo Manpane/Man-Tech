bf38d9e91e5824e517cfb7295fe33c68f915486a
Manpane <75156544+Manpane@users.noreply.github.com>
2022-11-08 15:57:41 +0545

    added rel noreferrer

--- src/components/AboutFeed.jsx
+++ src/components/AboutFeed.jsx
@@ -11,21 +11,21 @@ const AboutFeed = (props) => {
                 </p>
                 <h4 className="about-skills-header">My skills</h4>
                 <div className="about-skills-container skills-programming-languages">
-                    <div className="skill python"><a href="https://www.python.org/" target="_blank" >Python</a></div>
-                    <div className="skill rd"><a href="https://www.java.com/en/" target="_blank">Java</a></div>
-                    <div className="skill ylw"><a href="https://www.google.com/search?q=javascript" target="_blank">Javascript</a></div>
-                    <div className="skill org"><a href="https://www.google.com/search?q=html" target="_blank">HTML</a></div>
-                    <div className="skill lt_grn"><a href="https://www.google.com/search?q=css" target="_blank">CSS</a></div>
-                    <div className="skill skbl"><a href="https://www.google.com/search?q=C%2FC%2B%2B" target="_blank">C/C++</a></div>
-                    <div className="skill skbl"><a href="https://www.google.com/search?q=SQL" target="_blank">SQL</a></div>
+                    <div className="skill python"><a href="https://www.python.org/" rel="noreferrer" target="_blank" >Python</a></div>
+                    <div className="skill rd"><a href="https://www.java.com/en/" rel="noreferrer" target="_blank">Java</a></div>
+                    <div className="skill ylw"><a href="https://www.google.com/search?q=javascript" rel="noreferrer" target="_blank">Javascript</a></div>
+                    <div className="skill org"><a href="https://www.google.com/search?q=html" rel="noreferrer" target="_blank">HTML</a></div>
+                    <div className="skill lt_grn"><a href="https://www.google.com/search?q=css" rel="noreferrer" target="_blank">CSS</a></div>
+                    <div className="skill skbl"><a href="https://www.google.com/search?q=C%2FC%2B%2B" rel="noreferrer" target="_blank">C/C++</a></div>
+                    <div className="skill skbl"><a href="https://www.google.com/search?q=SQL" rel="noreferrer" target="_blank">SQL</a></div>
                 </div>
                 <div className="about-skills-container skills-platforms">
-                    <div className="skill"><a href="https://developer.android.com/" target="_blank">Android Development</a></div>
-                    <div className="skill"><a href="https://www.google.com/search?q=web+development" target="_blank">Web Development</a></div>
-                    <div className="skill"><a href="https://www.google.com/search?q=game+development" target="_blank">Game Development</a></div>
+                    <div className="skill"><a href="https://developer.android.com/" rel="noreferrer" target="_blank">Android Development</a></div>
+                    <div className="skill"><a href="https://www.google.com/search?q=web+development" rel="noreferrer" target="_blank">Web Development</a></div>
+                    <div className="skill"><a href="https://www.google.com/search?q=game+development" rel="noreferrer" target="_blank">Game Development</a></div>
                     <br/>
-                    <div className="skill"><a href="https://www.google.com/search?q=web+scrapping" target="_blank">Web Scraping</a></div>
-                    <div className="skill"><a href="https://www.google.com/search?q=automation" target="_blank">Automtation</a></div>
+                    <div className="skill"><a href="https://www.google.com/search?q=web+scrapping" rel="noreferrer" target="_blank">Web Scraping</a></div>
+                    <div className="skill"><a href="https://www.google.com/search?q=automation" rel="noreferrer" target="_blank">Automtation</a></div>
                 </div>
             </center>
         </div>
